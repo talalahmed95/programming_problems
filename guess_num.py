fb8ecbf706f2d4a42dfaca78402331b6a674544f
Talal Ahmed <tahmed530@gmail.com>
2021-12-28 15:57:21 +0500

    guess number second commit

--- guess_num.py
+++ guess_num.py
@@ -1,4 +1,38 @@
-num_to_guess = str(69)
+# num_to_guess = str(3)
+# num_to_guess_len = len(num_to_guess)
+# print("The number to guess is", num_to_guess_len, "digits")
+# matched = False
+# tries = 0
+# guess_list = []
+# while matched == False:
+#     num = int(input("Enter a number: "))
+#     num_to_guess = int(num_to_guess)
+#     guess_list.append(num)
+#     for i, val in enumerate(guess_list):
+#         # j = i + 1
+#         for j in range(i + 1, len(guess_list)):
+#         # for j, val2 in enumerate(guess_list):
+#             # j = i + 1
+#             # print("index of j:", j, ":: value of mj:", guess_list[j])
+#             # print("val:", val, ":: [j]:", guess_list[j])
+#             if val == guess_list[j]:
+#                 # print("i:", i, ":: val i:", val, ":::: j:", j, ":: val j:", guess_list[j])
+#                 # print("xxxxxxxxx INCREMENT xxxxxxxxxx")
+#                 tries += 1
+#
+#     if num > num_to_guess:
+#         print("guessed number is LARGER than the number to be guessed")
+#     elif num < num_to_guess:
+#         print("guessed number is SMALLER than the number to be guessed")
+#     else:
+#         # print("Congrats! your guess is correct. It took you", tries, "tries")
+#         print("Number of duplicates:", tries)
+#         matched = True
+
+
+
+
+num_to_guess = str(3)
 num_to_guess_len = len(num_to_guess)
 print("The number to guess is", num_to_guess_len, "digits")
 matched = False
@@ -8,21 +42,20 @@ while matched == False:
     num = int(input("Enter a number: "))
     num_to_guess = int(num_to_guess)
     guess_list.append(num)
-    print(guess_list)
-    for i, val in enumerate(guess_list):
-        # j = i + 1
-        for j in range(i + 1, len(guess_list)):
-            # j = i + 1
-            # print("index of j:", j, ":: value of j:", guess_list[j])
-            if val == guess_list[j]:
-                # print("i:", i, ":: val i:", val, ":::: j:", j, ":: val j:", guess_list[j])
-                print("** matched")
-                tries += 1
+    for i in guess_list:
+        if i not in guess_list:
+            guess_list[i] = 1
+        else:
+            guess_list[i] += 1
+
 
     if num > num_to_guess:
         print("guessed number is LARGER than the number to be guessed")
     elif num < num_to_guess:
         print("guessed number is SMALLER than the number to be guessed")
     else:
-        print("Congrats! your guess is correct. It took you", tries, "tries")
+        # print("Congrats! your guess is correct. It took you", tries, "tries")
+        print("Number of duplicates:", tries)
         matched = True
+
+
