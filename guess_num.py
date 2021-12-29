5adad86f2137171a3809ac4f2e7370c16bde0422
Talal Ahmed <tahmed530@gmail.com>
2021-12-29 16:08:44 +0500

    guess number third commit

--- guess_num.py
+++ guess_num.py
@@ -29,33 +29,37 @@
 #         print("Number of duplicates:", tries)
 #         matched = True
 
+# num_to_guess = str(3)
+# num_to_guess_len = len(num_to_guess)
+# print("The number to guess is", num_to_guess_len, "digits")
+# matched = False
+# tries = 0
+# guess_list = []
+
+# while matched == False:
+#     num = int(input("Enter a number: "))
+#     num_to_guess = int(num_to_guess)
+#     guess_list.append(num)
+#     for i in range(len(guess_list)):
+#         for j in range(i+1, len(guess_list)):
+#             tries += 1
+#
+#     if num > num_to_guess:
+#         print("guessed number is LARGER than the number to be guessed")
+#     elif num < num_to_guess:
+#         print("guessed number is SMALLER than the number to be guessed")
+#     else:
+#         # print("Congrats! your guess is correct. It took you", tries, "tries")
+#         print("Number of duplicates:", tries)
+#         matched = True
 
 
+guess_list = ['a', 'b', 'c', 'a', 'c', 'z', 'm', 'n']
+res = []
+tries = 1
+for i in range(len(guess_list)):
+    for j in range(i+1, len(guess_list)):
+        if guess_list[i] == guess_list[j]:
+            tries += 1
 
-num_to_guess = str(3)
-num_to_guess_len = len(num_to_guess)
-print("The number to guess is", num_to_guess_len, "digits")
-matched = False
-tries = 0
-guess_list = []
-while matched == False:
-    num = int(input("Enter a number: "))
-    num_to_guess = int(num_to_guess)
-    guess_list.append(num)
-    for i in guess_list:
-        if i not in guess_list:
-            guess_list[i] = 1
-        else:
-            guess_list[i] += 1
-
-
-    if num > num_to_guess:
-        print("guessed number is LARGER than the number to be guessed")
-    elif num < num_to_guess:
-        print("guessed number is SMALLER than the number to be guessed")
-    else:
-        # print("Congrats! your guess is correct. It took you", tries, "tries")
-        print("Number of duplicates:", tries)
-        matched = True
-
-
+print("Number of dup:", tries)
