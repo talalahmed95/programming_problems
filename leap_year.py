81ae100c53cae940b5dd38eebadb287b304f5d8d
Talal Ahmed <tahmed530@gmail.com>
2022-01-06 16:07:21 +0500

    leap year; add if not divisible by 4

--- leap_year.py
+++ leap_year.py
@@ -1,8 +1,37 @@
+# starting_year = 1
+# print("Starting from year", starting_year, ", ", end="")
+# input_year = int(input("Enter year to print the next 20 leap years: "))
+# leap_yr_list = []
+#
+# if (input_year % 4 == 0) and (input_year % 100 != 0) or (input_year % 400 == 0):
+#     for i in range(20):
+#         input_year += 4
+#         if (input_year % 4 == 0) and (input_year % 100 != 0) or (input_year % 400 == 0):
+#             leap_yr_list.append(input_year)
+#     print(leap_yr_list)
+# else:
+#     for x in range(5):
+#         for i in range(20):
+#             input_year += 4
+#             if (input_year % 4 == 0) and (input_year % 100 != 0) or (input_year % 400 == 0):
+#                 leap_yr_list.append(input_year)
+#         print(leap_yr_list)
+
+
 starting_year = 1
 print("Starting from year", starting_year, ", ", end="")
 input_year = int(input("Enter year to print the next 20 leap years: "))
 leap_yr_list = []
-for i in range(20):
+
+if input_year / 4 == 0.75:
+    input_year += 1
+elif input_year / 4 == 0.5:
+    input_year += 2
+elif input_year / 4 == 0.25:
+    input_year += 3
+
+print("input year:", input_year)
+for i in range(input_year, input_year + 20):
     input_year += 4
     if (input_year % 4 == 0) and (input_year % 100 != 0) or (input_year % 400 == 0):
         leap_yr_list.append(input_year)
